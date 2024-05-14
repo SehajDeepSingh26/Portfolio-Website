0e4d2e02bfeb514f37bf88bcbae77a55a076c854
Sehajdeep Singh <114660532+SehajDeepSingh26@users.noreply.github.com>
2024-05-15 01:50:41 +0530

    added chatbot

--- experience/script.js
+++ experience/script.js
@@ -32,14 +32,14 @@ srtop.reveal('.experience .timeline .container',{interval: 400});
 
 // Start of Tawk.to Live Chat
 var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/60df10bf7f4b000ac03ab6a8/1f9jlirg6';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
-})();
+    (function(){
+    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+    s1.async=true;
+    s1.src='https://embed.tawk.to/6643c3d507f59932ab3f929d/1htsb9uo9';
+    s1.charset='UTF-8';
+    s1.setAttribute('crossorigin','*');
+    s0.parentNode.insertBefore(s1,s0);
+    })();
 // End of Tawk.to Live Chat
 
 
@@ -65,7 +65,7 @@ document.onkeydown = function(e) {
 document.addEventListener('visibilitychange',
 function(){
     if(document.visibilityState === "visible"){
-        document.title = "Experience | Portfolio Jigar Sable";
+        document.title = "Experience | Portfolio Sehajdeep Singh";
         $("#favicon").attr("href","/assets/images/favicon.png");
     }
     else {
