4624831f9a55f8c79b6a6f9ce40fa3c8a0b26010
Sehajdeep Singh <114660532+SehajDeepSingh26@users.noreply.github.com>
2024-06-13 12:19:54 +0530

    experience page updates

--- projects/script.js
+++ projects/script.js
@@ -46,7 +46,7 @@ function showProjects(projects) {
     projects.forEach(project => {
         projectsHTML += `
         <div class="grid-item ${project.category}">
-        <div class="box tilt" style="width: 380px; margin: 2rem">
+        <div class="box tilt" style="width: 360px; margin: 1rem">
       <img draggable="false" src="/assets/images/projects/${project.image}.png" alt="project" />
       <div class="content">
         <div class="tag">
