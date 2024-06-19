38dc1be7d975800a0d172046f537311c1c690da9
Sehajdeep Singh <114660532+SehajDeepSingh26@users.noreply.github.com>
2024-06-19 17:13:32 +0530

    change visibility function

--- assets/js/script.js
+++ assets/js/script.js
@@ -60,7 +60,7 @@ document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
             document.title = "Portfolio | Sehajdeep Singh";
-            $("#favicon").attr("href", "assets/images/favicon.png");
+            $("#favicon").attr("href", "assets/images/hero.jpeg");
         }
         else {
             document.title = "Come Back To Portfolio";
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["frontend development", "backend development", "web development", "REST API"],
+    strings: ["Frontend development", "Backend development", "Web development", "REST API"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
